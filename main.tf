f3e92bb398d0b3ce00b19db8572edd0f09024b3a
rodrigofbmm <115274224+rodrigofbmm@users.noreply.github.com>
2025-05-29 17:10:58 +0200

    Update main.tf
    chage to use https instead of http, also implement certificate_arn and redirect from http -> https. www.x implement

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_security_group" "instance_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["x/32"]
+    cidr_blocks = ["2.155.93.197/32"]
     description = "SSH access from my IP only"
   }
 
@@ -157,16 +157,36 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
+
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.loadbalancer.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = 443
+  protocol          = "HTTPS"
+
+  certificate_arn = "arn:aws:acm:eu-west-1:586794460474:certificate/f76c3e82-9d12-4d47-89a2-efc8b6b22344"
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
   }
 }
+
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 
 resource "aws_lb_target_group_attachment" "attach" {
   target_group_arn = aws_lb_target_group.tg.arn
@@ -183,6 +203,18 @@ resource "aws_route53_zone" "main_zone" {
 resource "aws_route53_record" "root_domain" {
   zone_id = aws_route53_zone.main_zone.zone_id
   name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.loadbalancer.dns_name
+    zone_id                = aws_lb.loadbalancer.zone_id
+    evaluate_target_health = true
+  }
+}
+
+resource "aws_route53_record" "www_domain" {
+  zone_id = aws_route53_zone.main_zone.zone_id
+  name    = "www.${var.domain_name}"
   type    = "A"
 
   alias {
